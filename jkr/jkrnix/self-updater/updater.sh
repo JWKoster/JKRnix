21846b4e98194c240522db2a5091c75ca24b0e37
JelleKoster <jelle.w.koster@gmail.com>
2020-05-05 20:10:22 +0200

    updater fix

--- jkr/jkrnix/self-updater/updater.sh
+++ jkr/jkrnix/self-updater/updater.sh
@@ -1,5 +1,5 @@
 wget -qO ~/jkrnix.tar.gz https://github.com/JWKoster/JKRnix/tarball/master || exit 1
-tar -xzf ~/jkrnix.tar.gz --strip-components=1 && rm ~/jkrnix.tar.gz
+tar -xzf ~/jkrnix.tar.gz --strip-components=1 -C ~/ && rm ~/jkrnix.tar.gz
 wget -qO currentVersion.flat https://api.github.com/repos/JWKoster/JKRnix/commits/master --header="Accept: application/vnd.github.VERSION.sha"
-ln -s ${jkrnix}bin/*.sh ${jkr}bin/
-ln -s ${jkrnix}lib/*.shlib ${jkr}lib/
+ln -sf ${jkrnix}bin/*.sh ${jkr}bin/
+ln -sf ${jkrnix}lib/*.shlib ${jkr}lib/
