b55e3b96dbf66c0c95d92b3b37821e88b631d09a
JKR <jelle.koster@blujaysolutions.com>
2020-05-07 10:47:27 +0200

    updater.sh 2.0 using functions

--- jkr/jkrnix/self-updater/updater.sh
+++ jkr/jkrnix/self-updater/updater.sh
@@ -1,5 +1,26 @@
-wget -qO ~/jkrnix.tar.gz https://github.com/JWKoster/JKRnix/tarball/master || exit 1
+#!/bin/bash
+function jkrnix_install() {wget -qO ~/jkrnix.tar.gz https://github.com/JWKoster/JKRnix/tarball/master || exit 1
 tar -xzf ~/jkrnix.tar.gz --strip-components=1 -C ~/ && rm ~/jkrnix.tar.gz
 wget -qO currentVersion.flat https://api.github.com/repos/JWKoster/JKRnix/commits/master --header="Accept: application/vnd.github.VERSION.sha"
-ln -sf ${jkrnix}bin/*.sh ${jkr}bin/
-ln -sf ${jkrnix}lib/*.shlib ${jkr}lib/
+}
+
+function jkrnix_symlinks() {
+for sh in ${jkrnix}bin/*.sh
+do
+if [ -f ${sh} ]
+	then
+		ln -sf ${sh} ${jkr}bin/
+fi
+done
+
+for shlib in ${jkrnix}lib/*.shlib
+do
+if [ -f ${shlib} ]
+        then
+                ln -sf ${shlib} ${jkr}lib/
+fi
+done
+}
+
+jkrnix_install
+jkrnix_symlinks
