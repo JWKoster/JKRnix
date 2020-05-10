7dcc47c65d125edb476d39d8789d2e4c4c849466
JelleKoster <jelle.w.koster@gmail.com>
2020-05-10 16:08:59 +0200

    BUGFIX: version comparison

--- jkr/jkrnix/self-updater/checkForUpdates.sh
+++ jkr/jkrnix/self-updater/checkForUpdates.sh
@@ -2,15 +2,15 @@
 cd ${jkrnix}/self-updater/
 wget -qO gitVersion.flat https://api.github.com/repos/JWKoster/JKRnix/commits/master --header="Accept: application/vnd.github.VERSION.sha"
 
-if [ -n gitversion.flat ]
+if [ -z gitversion.flat ]
 then echo "GIT: NULL" > gitVersion.flat
 fi
 
-if [ -n currentVersion.flat ]
+if [ -z currentVersion.flat ]
 then echo "CURRENT: NULL" > currentVersion.flat
 fi
 
-if [ `cat gitVersion.flat` = `cat currentVersion.flat` ] ; 
+if [ "$(cat gitVersion.flat)" = "$(cat currentVersion.flat)" ] ; 
 	then
 		exit 0
 	else	
