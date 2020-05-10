41a89c04391e845af7be12c584d64b0215e1a536
JelleKoster <jelle.w.koster@gmail.com>
2020-05-10 10:41:09 +0200

    BUGFIX: Create version flat files if they don't exist.

--- jkr/jkrnix/self-updater/checkForUpdates.sh
+++ jkr/jkrnix/self-updater/checkForUpdates.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
 cd ${jkrnix}/self-updater/
 wget -qO gitVersion.flat https://api.github.com/repos/JWKoster/JKRnix/commits/master --header="Accept: application/vnd.github.VERSION.sha"
+
+if [ -n gitversion.flat ]
+then echo "GIT: NULL" > gitVersion.flat
+fi
+
+if [ -n currentVersion.flat ]
+then echo "CURRENT: NULL" > currentVersion.flat
+fi
 
 if [ `cat gitVersion.flat` = `cat currentVersion.flat` ] ; 
 	then
