38fdfe77f602c0937d961e62120d33845b3f4a8b
JKR <jelle.koster@blujaysolutions.com>
2020-06-06 17:47:07 +0200

    NEW: Template script can create standalone scripts using -s REWORK: Template script doesn't put anything in the target script it shouldn't.

--- jkr/jkrnix/bin/jkr_template_script.sh
+++ jkr/jkrnix/bin/jkr_template_script.sh
@@ -5,6 +5,21 @@
 # based on the work of many others, found through extensive google-fu
 # ==================================================================== #
 
+#START_EXCLUDE_ALWAYS
+# VS 2.0/01
+
+ 
+
+# History:
+
+# Version Date     		Who      Remarks
+
+# 1.0/01  2020/09/01	JKR      Created base version to more easily and uniformly create scripts
+# 2.0/01  2020/06/05	JKR		 Added functionality to create standalone scripts (-s), which can be deployed without dependency on jkrnix variables.
+# 2.0/02  2020/06/06	JKR		 Added functionality to exclude parts of this script from the resulting file using START & END EXCLUDE ALWAYS/JKR/STANDALONE
+
+#END_EXCLUDE_ALWAYS
+# ==================================================================== #
 # VS 1.0/01
 
  
@@ -15,23 +30,32 @@
 
 # 1.0/01  YYYY/MM/DD JKR        Created base version to ...
 
+# ==================================================================== #
+# Libraries
+
+#START_EXCLUDE_STANDALONE
 #Load libraries
 JKR_LIBS="jkr_usage.shlib jkr_logging.shlib"
 . ${jkr}/bin/jkr_load_lib.sh
-
+#END_EXCLUDE_STANDALONE
 #Globals
 
 gMyName=$(basename ${0})
 gHome=$( cd “$(dirname “$0”)” >/dev/null 2>&1 ; pwd -P )
 gSysDateTime=$(date +"%d_%m_%Y_%H_%M_%S")
-gLogDir=${jkr}/logs/
+if [ -d ${jkr}/logs ] 
+	then gLogDir=${jkr}/logs/
+	else gLogDir=$(mktemp -d) || exit 1
+fi
 gLog=${gLogDir}/${gMyName}_${gSysDateTime}.log
-
+#START_EXCLUDE_ALWAYS
+gStandalone="false"
+#END_EXCLUDE_ALWAYS
 
 # ==================================================================== #
 # Options and manual
-# ==================================================================== #
-while getopts ":hvD" Flag
+
+while getopts ":hvDs" Flag
 #Some default flags
 do
 
@@ -53,10 +77,16 @@ do
                         usage
                         exit
                         ;;
+
                 v)      # Verbose!
                         set_verbose
                         ;;
-                
+					#START_EXCLUDE_ALWAYS
+				s)		#Creates a standalone script without dependency on jkrnix, inserting the libraries' current versions.
+						gStandalone="true"
+						verbose "Creating script as standalone."
+						;;
+					#END_EXCLUDE_ALWAYS
                 D)      # Debug to stderr, same as "sh -x"
                         set -x
                         ;;
@@ -74,23 +104,25 @@ done
         # Now shift all flagged parameters to receive the non flagged values as input
    shift $((OPTIND-1))
 
-#HELPSTART#
+#DOCSTART
 #
 #Document your script here!
+#START_EXCLUDE_ALWAYS
 #Documentation for jkr_template_script.sh:
 # jkr_template_script.sh copies itself to your desired new script name, so it can serve as a basis for your new script.
 # jkr_template_script.sh [TARGET FILE NAME]
 # For example: jkr_template_script.sh copied.sh
-#HELPEND
-# ==================================================================== #
+#END_EXCLUDE_ALWAYS
+#DOCEND
 
 # ==================================================================== #
 # Functions
-# ==================================================================== #
 
 # ==================================================================== #
 # Body
-# ==================================================================== #
+
+#START_EXCLUDE_ALWAYS
+#Prevent losing work.
 echo ${1}
 if [ -f ${1} ]
 	then 
@@ -100,4 +132,30 @@ if [ -f ${1} ]
 		fi
 fi
 
-cp $gHome/${gMyName} ${1}
+cp ${gHome}/${gMyName} ${1}
+
+#Clean up the workings of this script, and include the libraries in case it is standalone.
+if [ ${gStandalone} = "true" ] 
+	then 
+		sed -i '/^\#START_EXCLUDE_STANDALONE/,/^\#END_EXCLUDE_STANDALONE/d' ${1}
+		tmpFile=$(mktemp -p ${gLogDir}) || exit 1
+		echo "#Start of imported libraries, to make this script standalone." >> ${tmpFile}
+		echo "#It may not look pretty, but does allow a more flexible creation. Clean it up if you want." >> ${tmpFile}
+		for i in ${JKR_LIBS}
+		do cat ${jkr}/lib/${i} >> ${tmpFile}
+		done
+		echo "#End of imported libraries" >> ${tmpFile}
+		sed -e '/Libraries/r ${tmpFile}' ${1}
+	else 
+		sed -i '/^\#START_EXCLUDE_JKR/,/^\#END_EXCLUDE_JKR/d' ${1}
+fi
+sed -i '/^\#START_EXCLUDE_ALWAYS/,/^\#END_EXCLUDE_ALWAYS/d' ${1}
+
+#Set permissions: jkrnix scripts should be adjusted through GIT, standalone scripts can be adjusted ad hoc.
+if [ ${gStandalone} = "true" ]
+	then chmod 710 ${1}
+	else chmod 510 ${1}
+fi
+
+verbose "${1} has been created succesfully".
+#END_EXCLUDE_ALWAYS
