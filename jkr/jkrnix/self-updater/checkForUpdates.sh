43f13dc8992684858b76f0124a115de3226a0c3b
JelleKoster <jelle.w.koster@gmail.com>
2020-06-20 11:20:22 +0200

    FIX: inconsistency in URL usage

--- jkr/jkrnix/self-updater/checkForUpdates.sh
+++ jkr/jkrnix/self-updater/checkForUpdates.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-export jkrnixrepo='https://api.github.com/repos/JWKoster/JKRnix/commits/master'
 
-cd ${jkrnix}/self-updater/
-wget -qO gitVersion.flat ${jkrnixrepo} --header="Accept: application/vnd.github.VERSION.sha"
+export jkrnixrepo='https://github.com/JWKoster/JKRnix/tarball/master'
+export jkrnixrepoversion='https://api.github.com/repos/JWKoster/JKRnix/commits/master'
 
 if [ -z gitversion.flat ]
 then echo "GIT: NULL" > gitVersion.flat
@@ -12,10 +11,12 @@ if [ -z currentVersion.flat ]
 then echo "CURRENT: NULL" > currentVersion.flat
 fi
 
+cd ${jkrnix}/self-updater/
+wget -qO gitVersion.flat ${jkrnixrepoversion} --header="Accept: application/vnd.github.VERSION.sha"
+
 if [ "$(cat gitVersion.flat)" = "$(cat currentVersion.flat)" ] ; 
 	then
 		exit 0
 	else	
 		sh updater.sh
 fi
-cd -
