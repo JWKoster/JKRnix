e451728988f88810256d4a1176ac170e5b75d2b1
JKR <jelle.koster@blujaysolutions.com>
2021-09-08 09:07:42 +0200

    Fix: tpt script, determining log and cfg

--- jkr/jkrnix/bin/jkr_template_script.sh
+++ jkr/jkrnix/bin/jkr_template_script.sh
@@ -18,6 +18,7 @@
 # 2.0/01  2020/06/05	JKR		 Added functionality to create standalone scripts (-s), which can be deployed without dependency on jkrnix variables.
 # 2.0/02  2020/06/06	JKR		 Added functionality to exclude parts of this script from the resulting file using START & END EXCLUDE ALWAYS/JKR/STANDALONE
 # 2.0/03  2020/07/30	JKR		 Added create_myconfig function, purposely not through shlib so the heredoc can be changed per script
+# 2.0/04  2021/08/16    JKR      Some polish on determining the log and cfg dir.
 #END_EXCLUDE_ALWAYS
 # ==================================================================== #
 # VS 1.0/01
@@ -43,25 +44,33 @@ if hash ${gMyName} 2>/dev/null
 fi
 
 gSysDateTime=$(date +"%d_%m_%Y_%H_%M_%S")
-if [ -d ${jkr}/logs ] 
-	then gLogDir=${jkr}/logs/
-	else gLogDir=$(mktemp -d) || exit 1
+
+if [ -z ${jkr} ]
+        then
+                if [ -d ~/jkr ]
+                        then jkr=~/jkr/
+                fi
+fi
+
+if [ ! -z ${jkr} ]
+        then
+                if [ -d ${jkr}/logs ]
+                        then gLogDir=${jkr}/logs/
+                        else mkdir ${jkr}/logs
+                             gLogDir=${jkr}/logs/
+                fi
+        else gLogDir=$(mktemp -d) || exit 1
 fi
 gLog=${gLogDir}/${gMyName}_${gSysDateTime}.log
-#START_EXCLUDE_STANDALONE
-gMyConfigFile=${jkr}/cfg/${gMyName%.*}.cfg
-#END_EXCLUDE_STANDALONE
-#START_EXCLUDE_JKR
-if [ ! -d ~/jkr ]
-	then
-		mkdir ~/jkr
-		if [ ! -d ~/jkr/cfg ]
-			then
-				mkdir ~/jkr/cfg
-		fi
+
+if [ ! -z "${jkr}" ]
+        then gMyConfigFile=${jkr}/cfg/${gMyName%.*}.cfg
+        elif [ -d ~/jkr ]
+                then gMyConfigFile=~/jkr/cfg/${gMyName%.*}.cfg
+        else warning "Cannot determine correct location for config dir. '${jkr}' or ~/jkr do not exist."
+             warning "This is only a problem if this script uses a config file."
 fi
-gMyConfigFile=~/jkr/cfg/${gMyName%.*}.cfg
-#END_EXCLUDE_JKR
+
 #START_EXCLUDE_ALWAYS
 gStandalone="false"
 #END_EXCLUDE_ALWAYS
@@ -74,6 +83,47 @@ gStandalone="false"
 JKR_LIBS="jkr_usage.shlib jkr_logging.shlib"
 . ${jkr}/bin/jkr_load_lib.sh
 #END_EXCLUDE_STANDALONE
+
+# ==================================================================== #
+# Functions
+
+function create_myconfig()
+{
+# Create my own config file.
+if [ ! -d ~/jkr ]
+	then
+		mkdir ~/jkr
+		if [ ! -d ~/jkr/cfg ]
+			then
+				mkdir ~/jkr/cfg
+		fi
+fi
+# Check if config file already exists:
+if [ -f "${gMyConfigFile}" ]
+then
+        complain "Config file already exists. Aborting."
+        return 1
+fi
+
+# Check if directory exists:
+typeset tMyConfigDir=$(dirname ${gMyConfigFile})
+
+if [ ! -d "${gMyConfigFile}" ]
+then
+        mkdir -p ${tMyConfigDir} || return 1
+fi
+
+# Now create the config file (remember to escape variables you want to be passed on using \ before $):
+cat <<-EOC > ${gMyConfigFile}
+# Configuration file ${gMyName}
+exampleParameter="exampleString" #Describe the parameter
+EOC
+
+verbose "Config file created: ${gMyConfigFile}"
+verbose "Change this config file first before continue the installation."
+return 0
+}
+
 
 # ==================================================================== #
 # Options and documentation
@@ -138,39 +188,6 @@ done
 # For example: jkr_template_script.sh copied.sh
 #END_EXCLUDE_ALWAYS
 #DOCEND
-
-# ==================================================================== #
-# Functions
-
-function create_myconfig()
-{
-# Create my own config file.
-
-# Check if config file already exists:
-if [ -f "${gMyConfigFile}" ]
-then
-        complain "Config file already exists. Aborting."
-        return 1
-fi
-
-# Check if directory exists:
-typeset tMyConfigDir=$(dirname ${gMyConfigFile})
-
-if [ ! -d "${gMyConfigFile}" ]
-then
-        mkdir -p ${tMyConfigDir} || return 1
-fi
-
-# Now create the config file (remember to escape variables you want to be passed on using \ before $):
-cat <<-EOC > ${gMyConfigFile}
-# Configuration file ${gMyName}
-exampleParameter="exampleString" #Describe the parameter
-EOC
-
-verbose "Config file created: ${gMyConfigFile}"
-verbose "Change this config file first before continue the installation."
-return 0
-}
 
 # ==================================================================== #
 # Body
