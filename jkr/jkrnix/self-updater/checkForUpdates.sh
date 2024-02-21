7ea1361188d457bb5a2be168d0cb30d69b8a171e
Jelle Koster <33488519+JelleKosterBluJay@users.noreply.github.com>
2024-02-21 10:22:14 +0100

    CHANGE: checkForUpdates.sh - confirmation

--- jkr/jkrnix/self-updater/checkForUpdates.sh
+++ jkr/jkrnix/self-updater/checkForUpdates.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+JKR_LIBS="jkr_logging.shlib"
+. jkr_load_lib.sh
 
 export jkrnixrepo='https://github.com/JWKoster/JKRnix/tarball/master'
 export jkrnixrepoversion='https://api.github.com/repos/JWKoster/JKRnix/commits/master'
@@ -14,11 +16,14 @@ fi
 cd ${jkrnix}/self-updater/
 wget -qO ${jkrnix}/self-updater/gitVersion.flat ${jkrnixrepoversion} --header="Accept: application/vnd.github.VERSION.sha"
 
-if [ "$(cat ${jkrnix}/self-updater/gitVersion.flat)" = "$(cat ${jkrnix}/self-updater/currentVersion.flat)" ] ; 
-	then
+if [ "$(cat ${jkrnix}/self-updater/gitVersion.flat)" = "$(cat ${jkrnix}/self-updater/currentVersion.flat)" ] ;
+        then
         echo "JKRnix is active. No updates to be done. Enjoy!"
             exit 0
-    else
-            sh updater.sh && echo "JKRnix updated to the latest version. Enjoy!"
+        else
+        if confirm "JKRnix: There's an update available. Do you want to update? Y/N"
+                then
+                sh updater.sh && echo "JKRnix updated to the latest version. Enjoy!"
+        fi
 
 fi
